2ebbee7cfee2b76d40982ca3cb17eeb1981e01f3
alan.amanco <alan.amanco@sigep.com.br>
2023-04-24 13:42:09 -0300

    detalhes receitas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food/screens/categories_meals_screen.dart';
 import 'package:food/screens/categories_screen.dart';
+import 'package:food/screens/meal_detail.screen.dart';
 import 'package:food/utils/app_routes.dart';
 
 void main() => runApp(const MyApp());
@@ -24,6 +25,7 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (context) => const CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (context) => const CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (context) => const MealDetailScreen(),
       },
     );
   }
--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,4 +1,7 @@
+import 'package:food/models/meal.dart';
+
 class AppRoutes {
   static const HOME = '/';
   static const CATEGORIES_MEALS = '/categories-meals';
+  static const MEAL_DETAIL = '/meal-detael';
 }
